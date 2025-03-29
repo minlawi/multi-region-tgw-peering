292d02d9588305962286a460e109b97fa69d2cdb
minlawi <minlawi1992@gmail.com>
2025-03-29 20:22:50 +0800

    Updated sg_tgw.tf

--- sg_tgw.tf
+++ sg_tgw.tf
@@ -1,5 +1,5 @@
 # Create a TGW in the SG region
-resource "aws_ec2_transit_gateway" "sg_tgw" {
+resource "aws_ec2_transit_gateway" "tgw_sg" {
   count                           = var.create_vpc ? 1 : 0
   default_route_table_association = "disable"
   default_route_table_propagation = "disable"
@@ -13,44 +13,44 @@ resource "aws_ec2_transit_gateway" "sg_tgw" {
 # Create a TGW Route Table in the SG region
 resource "aws_ec2_transit_gateway_route_table" "tgw_rtb_sg" {
   count              = var.create_vpc ? 1 : 0
-  transit_gateway_id = aws_ec2_transit_gateway.sg_tgw[0].id
+  transit_gateway_id = aws_ec2_transit_gateway.tgw_sg[0].id
   tags = {
-    Name      = "sg-tgw-rtb"
+    Name      = "tgw-rtb-sg"
     Terraform = "true"
   }
 
 }
 
 # Create a TGW Attachment for the VPC in the SG region
-resource "aws_ec2_transit_gateway_vpc_attachment" "tgwa_sg_vpc_a" {
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgwa_vpc_sg" {
   count              = var.create_vpc ? 1 : 0
-  vpc_id             = aws_vpc.sg_vpc[0].id
-  subnet_ids         = tolist(aws_subnet.priv_subnets_sg_tgw[*].id)
-  transit_gateway_id = aws_ec2_transit_gateway.sg_tgw[0].id
+  vpc_id             = aws_vpc.vpc_sg[0].id
+  subnet_ids         = tolist(aws_subnet.priv_subnets_tgw_sg[*].id)
+  transit_gateway_id = aws_ec2_transit_gateway.tgw_sg[0].id
   tags = {
-    Name      = "sg-tgw-attachment"
+    Name      = "tgw-attachment-sg"
     Terraform = "true"
   }
 }
 
 # Associate the TGW Attachment with the TGW Route Table in the SG region
-resource "aws_ec2_transit_gateway_route_table_association" "tgw_rtb_assoc" {
+resource "aws_ec2_transit_gateway_route_table_association" "tgw_rtb_assoc_sg" {
   count                          = var.create_vpc ? 1 : 0
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgwa_sg_vpc_a[0].id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgwa_vpc_sg[0].id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rtb_sg[0].id
 }
 
 # Propagate the TGW Attachment in the TGW Route Table in the SG region
-resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_rtb_propagation" {
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_rtb_propagation_sg" {
   count                          = var.create_vpc ? 1 : 0
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgwa_sg_vpc_a[0].id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgwa_vpc_sg[0].id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rtb_sg[0].id
 }
 
 # Add "0.0.0.0/0" CIDR in the TGW Route Table in the SG region
-resource "aws_ec2_transit_gateway_route" "tgw_route_sg_vpc" {
+resource "aws_ec2_transit_gateway_route" "tgw_route_vpc_sg" {
   count                          = var.create_vpc ? 1 : 0
   destination_cidr_block         = "0.0.0.0/0"
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgwa_sg_vpc_a[0].id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgwa_vpc_sg[0].id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rtb_sg[0].id
 }
